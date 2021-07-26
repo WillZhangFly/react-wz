c8fe9aef7803f0f27768d9cfaff3d0e69d1fcbfa
WillZhangFly <oliverottawa@gmail.com>
2021-07-25 23:31:37 -0400

    tasklist reformat

--- src/app/components/TaskList.jsx
+++ src/app/components/TaskList.jsx
@@ -1,29 +1,26 @@
 import React from 'react';
-import { connect } from "react-redux";
+import { connect } from 'react-redux';
 
-export const TaskList= ({tasks , name}) => {
-    return (
-        <div className="card p-2 m-2">
-                    <h2>
-            {name}
-        </h2>
-        <div>
-            {tasks.map(task =>(<div>{task.name}</div>))}
-        </div>
-        </div>
+export const TaskList = ({ tasks, name }) => {
+  return (
+    <div className="card p-2 m-2">
+      <h2>{name}</h2>
+      <div>
+        {tasks.map((task) => (
+          <div>{task.name}</div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-    )
-}
-
-const mapStateToProps = (state , ownProps) => {
-    let groupID = ownProps.id;
-    return {
-        name:ownProps.name,
-        id: groupID,
-        tasks: state.tasks.filter(task => task.group === groupID)
-    }
-}
+const mapStateToProps = (state, ownProps) => {
+  let groupID = ownProps.id;
+  return {
+    name: ownProps.name,
+    id: groupID,
+    tasks: state.tasks.filter((task) => task.group === groupID),
+  };
+};
 
 export const ConnectedTaskList = connect(mapStateToProps)(TaskList);
-
-
