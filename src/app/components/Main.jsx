414dc8cbb38efd7039d334ce42b289f70c2d7515
WillZhangFly <oliverottawa@gmail.com>
2021-07-26 19:48:38 -0400

    set up navigation bar

--- src/app/components/Main.jsx
+++ src/app/components/Main.jsx
@@ -2,17 +2,25 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { ConnectedDashboard } from './Dashboard';
 import { store } from '../store';
-import {Router, Route} from "react-router-dom";
-import { history } from "../store/history";
+import { Router, Route, Switch } from 'react-router-dom';
+import { history } from '../store/history';
+import { ConnectedNavigation } from './Navigation';
 
 export default function Main() {
   return (
     <Router history={history}>
-          <Provider store={store}>
-      <div>
-        <Route exact path="/dashboard" render={() => (<ConnectedDashboard />)} />
-      </div>
-    </Provider>
+      <Provider store={store}>
+        <div className="container mt-3">
+          <ConnectedNavigation />
+          <Switch>
+            <Route
+              exact
+              path="/dashboard"
+              render={() => <ConnectedDashboard />}
+            />
+          </Switch>
+        </div>
+      </Provider>
     </Router>
   );
 }
