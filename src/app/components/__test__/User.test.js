048b16c2faec24db2a01cd1ee63e0d7883918515
WillZhangFly <oliverottawa@gmail.com>
2021-08-04 00:32:43 -0400

    test case for user wrapped in a describe

--- src/app/components/__test__/User.test.js
+++ src/app/components/__test__/User.test.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { User } from "../User";
 
-it("User component renders without crashing", () => {
-  ReactDOM.render(<User name="abc" />, document.createElement("div"));
+describe("User component ", () => {
+  it("User component renders without crashing", () => {
+    ReactDOM.render(<User name="abc" />, document.createElement("div"));
+  });
 });
