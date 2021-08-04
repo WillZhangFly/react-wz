aaf2946c57a5db422ef3624059e91732d1180a2a
WillZhangFly <oliverottawa@gmail.com>
2021-08-04 00:11:00 -0400

    update user component jest test cases

--- src/app/components/__test__/User.test.js
+++ src/app/components/__test__/User.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { User } from "./User";
+import { User } from "../User";
 
 it("User component renders without crashing", () => {
   ReactDOM.render(<User name="abc" />, document.createElement("div"));
