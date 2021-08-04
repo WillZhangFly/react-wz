058c8f6e11477f12a835d469a10fbd62f54453df
WillZhangFly <oliverottawa@gmail.com>
2021-08-03 23:23:33 -0400

    add jest testing

--- src/server/server.spec.js
+++
@@ -1,6 +0,0 @@
-import { addNewTask, updateTask } from "./server";
-
-(async function mySpec() {
-  await addNewTask({ name: "Will Task", id: "1234567" });
-  await updateTask({ name: "Will Task updated", id: "1234567" });
-})();
